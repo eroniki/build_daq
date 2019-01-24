c2ec8e18772db5205a5f0224889cc77b9d8789f7
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2019-01-24 18:21:18 -0500

    Clean-up the app.py file

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import json
 import sys
 import time
 import os
+
+import flask
 from flask import Flask, render_template, Response, request, make_response
 
 import flask_login
-import flask
 
 from camera import VideoCamera
 from pose_detection import pose_detection
@@ -29,10 +30,23 @@ class flask_app(object):
         self.rooms = self.load_rooms()
         self.app = Flask(__name__)
         self.app.secret_key = key
-        # self.login_manager = flask_login.LoginManager()
-        # self.login_manager.init_app(self.app)
-        # self.login_manager.user_loader(self.user_loader)
-        # self.login_manager.request_loader(self.request_loader)
+        self.login_manager = flask_login.LoginManager()
+        self.login_manager.init_app(self.app)
+        self.login_manager.user_loader(self.user_loader)
+        self.login_manager.unauthorized_handler(self.unauthorized_handler)
+        self.login_manager.request_loader(self.request_loader)
+
+        handler = RotatingFileHandler(
+            'logs/status.log', maxBytes=10000, backupCount=99)
+        formatter = logging.Formatter(
+            "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+        handler.setLevel(logging.DEBUG)
+        handler.setFormatter(formatter)
+        self.app.logger.addHandler(handler)
+
+        log = logging.getLogger('werkzeug')
+        log.setLevel(logging.DEBUG)
+        log.addHandler(handler)
 
         self.create_endpoints()
 
@@ -45,7 +59,17 @@ class flask_app(object):
         return rooms
 
     def create_endpoints(self):
-        self.app.add_url_rule("/", "index", self.index)
+        self.app.add_url_rule("/",
+                              "index",
+                              self.index)
+
+        self.app.add_url_rule("/video/<room_name>",
+                              "video",
+                              self.video)
+
+        self.app.add_url_rule("/video_feed/<room_name>",
+                              "video_feed",
+                              self.video_feed)
 
         self.app.add_url_rule("/check_experiment/<int:id>",
                               "check_experiment",
@@ -59,6 +83,10 @@ class flask_app(object):
                               "list_experiments",
                               self.list_experiments)
 
+        self.app.add_url_rule("/delete_experiment/<exp_id>",
+                              "delete_experiment",
+                              self.delete_experiment)
+
         self.app.add_url_rule("/status_rooms/<room_name>",
                               "status_room",
                               self.status_room)
@@ -67,61 +95,115 @@ class flask_app(object):
                               "status_rooms",
                               self.status_all_rooms)
 
-        # self.app.add_url_rule("/login",
-        #                       "login",
-        #                       self.login)
-        #
-        # self.app.add_url_rule("/logout",
-        #                       "logout",
-        #                       self.logout)
+        self.app.add_url_rule("/login",
+                              "login",
+                              self.login,
+                              methods=['GET', 'POST'])
+
+        self.app.add_url_rule("/logout",
+                              "logout",
+                              self.logout)
+
+        self.app.add_url_rule("/log",
+                              "log",
+                              self.log)
+
+        self.app.add_url_rule("/log<int:n>",
+                              "logn",
+                              self.log_n)
+
+        self.app.add_url_rule("/test_camera/<cam_id>",
+                              "test_camera",
+                              self.test_camera)
 
         self.app.view_functions['index'] = self.index
+        self.app.view_functions['video'] = self.video
+        self.app.view_functions['video_feed'] = self.video_feed
         self.app.view_functions['check_experiment'] = self.check_experiment
         self.app.view_functions['clone'] = self.clone
         self.app.view_functions['list_experiments'] = self.list_experiments
+        self.app.view_functions['delete_experiment'] = self.delete_experiment
         self.app.view_functions['status_room'] = self.status_room
         self.app.view_functions['status_rooms'] = self.status_all_rooms
 
-        # self.app.view_functions['login'] = self.login
-        # self.app.view_functions['logout'] = self.logout
+        self.app.view_functions['login'] = self.login
+        self.app.view_functions['logout'] = self.logout
 
-    # @flask_login.login_required
+        self.app.view_functions['log'] = self.log
+        self.app.view_functions['log_n'] = self.log_n
+
+    def index(self):
+        return render_template('index.html')
+
+    def video(self, room_name):
+        return render_template('video.html', user=room_name)
+
+    def video_feed(self, room_name):
+        if room_name.lower() == "cears":
+            room_id = 1
+        elif room_name.lower() == "computer_lab":
+            room_id = 0
+        else:
+            return "Wrong Room ID is selected!"
+
+        exp_id = str(int(time.time() * 1000))
+        devices = self.rooms[room_id]["devices"]
+        return Response(self.gen(VideoCamera(devices, exp_id)),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    def gen(self, camera):
+        img_id = 0
+        while True:
+            frame = camera.get_all_frames(img_id)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+            img_id += 1
+
+    @flask_login.login_required
     def check_experiment(self, id):
         cam_statement = str()
         for i in range(7):
-            fname = os.path.join("data/", str(id), "/", str(i)+"/")
+            fname = os.path.join("data/", str(id), "/", str(i) + "/")
             if os.path.exists(fname):
                 n = len(pd.find_images(fname))
             else:
                 n = 0
             cam_statement += "Camera {i}: {n} images found! ".format(i=i, n=n)
 
-        date = self.um.timestamp_to_date(id/1000)
+        date = self.um.timestamp_to_date(id / 1000)
         exp = {"timestamp": id,
                "date": date,
                "camera": cam_statement,
                "room": "room"}
         return render_template('experiment.html', user=exp)
 
-    def index(self):
-        return render_template('index.html')
-
-    # @flask_login.login_required
+    # TODO: Implement this
+    @flask_login.login_required
     def clone(self, id):
         return str(id)
 
+    @flask_login.login_required
     def list_experiments(self):
         subfolders = self.um.list_subfolders("data/*/")
         experiments = self.um.list_experiments(subfolders)
         statement = ""
         for exp in experiments:
             try:
-                date = self.um.timestamp_to_date(int(exp)/1000)
+                date = self.um.timestamp_to_date(int(exp) / 1000)
                 statement += "<a href=check_experiment/{exp}>{exp} - ({date})</a><br />".format(
                     exp=exp, date=date)
             except:
                 print "Skipped"
+
+        if statement == "":
+            statement = "No experiments conducted yet!"
         return statement
+
+    def delete_experiment(self, exp_id):
+        folder = self.um.experiment_path(exp_id)
+        self.um.delete_folder(folder)
+
+        return "OK"
 
     def status_room(self, room_name):
         if room_name.lower() == "cears":
@@ -131,7 +213,8 @@ class flask_app(object):
         else:
             return "Wrong room id"
         devices = self.rooms[room_id]["devices"]
-        statement_all = "{n} cameras placed here. <br />".format(n=len(devices))
+        statement_all = "{n} cameras placed here. <br />".format(
+            n=len(devices))
         for dev_id, dev in enumerate(devices):
             if os.path.exists(dev):
                 statement = "Device {dev_id} found. {dev}<br />".format(
@@ -171,14 +254,17 @@ class flask_app(object):
                    '''
 
         email = request.form.get('email', None)
-        password = hashlib.md5(request.form.get('password', None)).hexdigest().upper()
+        password = request.form.get('password', None)
+        password = self.um.get_md5hash(password)
+
+        print email, password
 
         if email in self.users:
             if self.users[email]['password'] == password:
                 user = User()
                 user.id = email
                 flask_login.login_user(user)
-                return flask.redirect(flask.url_for('protected'))
+                return flask.redirect("/")
             else:
                 return "wrong password"
         else:
@@ -190,23 +276,19 @@ class flask_app(object):
         flask_login.logout_user()
         return 'Logged out'
 
-    # @flask_login.login_required
+    @flask_login.login_required
     def protected():
         return 'Logged in as: ' + flask_login.current_user.id
 
-    class User(flask_login.UserMixin):
-        pass
-
-    # @login_manager.user_loader
     def user_loader(self, email):
         if email not in self.users:
             return
+        print "loader calisti"
 
         user = User()
         user.id = email
         return user
 
-    # @login_manager.request_loader
     def request_loader(self, request):
         email = request.form.get('email')
         if email not in self.users:
@@ -220,22 +302,24 @@ class flask_app(object):
         user.is_authenticated = request.form['password'] == self.users[email]['password']
 
         return user
-#
-# @app.route("/pose_detection/<exp_id>/<camera_id>/<img_id>")
 
-    # def pose(self, exp_id, camera_id, img_id):
-    #     pd = pose_detection()
-    #     fname = os.path.join("data", exp_id, camera_id, img_id+".png")
-    #     pose = pd.detect_pose(fname)
-    #     if isinstance(pose, str):
-    #         return pose
-    #     else:
-    #         retval, buffer = cv2.imencode('.png', pose)
-    #         print buffer.shape
-    #         response = make_response(buffer.tobytes())
-    #         response.headers['Content-Type'] = 'image/png'
-    #     return response
-#
+    def unauthorized_handler(self):
+        return 'Unauthorized user!'
+
+# @app.route("/pose_detection/<exp_id>/<camera_id>/<img_id>")
+# def pose(self, exp_id, camera_id, img_id):
+#     pd = pose_detection()
+#     fname = os.path.join("data", exp_id, camera_id, img_id+".png")
+#     pose = pd.detect_pose(fname)
+#     if isinstance(pose, str):
+#         return pose
+#     else:
+#         retval, buffer = cv2.imencode('.png', pose)
+#         print buffer.shape
+#         response = make_response(buffer.tobytes())
+#         response.headers['Content-Type'] = 'image/png'
+#     return response
+
 # @app.route("/pose_detection/<exp_id>/<camera_id>")
 # def pose_all_images(exp_id, camera_id):
 #     pd = pose_detection()
@@ -273,156 +357,37 @@ class flask_app(object):
 #     return "{n} number of images were processed!".format(n=n)
 #
 #
-# @app.route("/status/<room_name>", methods=["POST", "GET"])
-# def status_cears(room_name):
-#     if room_name.lower() == "cears":
-#         room_id = 1
-#     else:
-#         room_id = 0
-#     devices = app.config["rooms"][room_id]["devices"]
-#     statement_all = "{n} cameras placed here. <br />".format(n=len(devices))
-#     for dev_id, dev in enumerate(devices):
-#         if os.path.exists(dev):
-#             statement = "Device {dev_id} found. {dev}<br />".format(
-#                 dev_id=dev_id, dev=dev)
-#         else:
-#             statement = "Device {dev_id} NOT found. {dev}<br />".format(
-#                 dev_id=dev_id, dev=dev)
-#         statement_all += statement
-#     return statement_all
-#
-#
-# @app.route("/status", methods=["POST", "GET"])
-# def status_all():
-#     statements = str()
-#     for room_id in range(2):
-#         print room_id
-#         devices = app.config["rooms"][room_id]["devices"]
-#         statement_all = "Room Name: {rm}<br />{n} cameras placed here. <br />".format(
-#             rm=app.config["rooms"][room_id]["name"], n=len(devices))
-#         for dev_id, dev in enumerate(devices):
-#             if os.path.exists(dev):
-#                 statement = "Device {dev_id} found. {dev}<br />".format(
-#                     dev_id=dev_id, dev=dev)
-#             else:
-#                 statement = "Device {dev_id} NOT found. {dev}<br />".format(
-#                     dev_id=dev_id, dev=dev)
-#             statement_all += statement
-#
-#         statements += statement_all
-#     return statements
-#
-#
-# @app.route('/video/<room_name>', methods=['GET', 'POST'])
-# def video(room_name):
-#     if room_name.lower() == "cears":
-#         app.config["room_id"] = 1
-#     else:
-#         app.config["room_id"] = 0
-#     return render_template('video.html')
-#
-#
-#
-# @app.route('/video_feed')
-# def video_feed():
-#     room_id = app.config["room_id"]
-#     if room_id is None:
-#         room_id = 0
-#     else:
-#         room_id = int(room_id)
-#     exp_id = str(int(time.time()*1000))
-#     devices = app.config["rooms"][room_id]["devices"]
-#     return Response(gen(VideoCamera(devices, exp_id)),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
-#
-# @app.route('/test_camera/<cam_id>')
-# def test_camera(cam_id):
-#     return cam_id
-#
+    def test_camera(self, cam_id):
+        # TODO: Implement this
+        return cam_id
 
-#
-# @app.route('/log')
-# def log():
-#     lines = tailer.tail(open('logs/status.log'), 10)
-#
-#     statement = ""
-#
-#     for line in lines:
-#         statement += (line + "<br />")
-#     return statement
-#
-#
-# @app.route('/log/<int:n>')
-# def log_n(n):
-#     lines = tailer.tail(open('logs/status.log'), n)
-#
-#     statement = ""
-#
-#     for line in lines:
-#         statement += (line + "<br />")
-#     return statement
-#
-# #
-# # @app.route('/delete_experiment/<exp_id>')
-# # def delete_experiment(exp_id):
-# #     u = utils.misc()
-# #     folder = u.experiment_path(exp_id)
-# #     u.delete_folder(folder)
-# #
-# #     return "OK"
-#
-# def gen(camera):
-#     img_id = 0
-#     while True:
-#         frame = camera.get_all_frames(img_id)
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-#         img_id += 1
-#
-#
-#
-#
-# @app.route('/logout')
-# def logout():
-#     flask_login.logout_user()
-#     return 'Logged out'
-#
-# @login_manager.unauthorized_handler
-# def unauthorized_handler():
-#     return 'Unauthorized'
-#
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if flask.request.method == 'GET':
-#         return '''
-#                <form action='login' method='POST'>
-#                 <input type='text' name='email' id='email' placeholder='email'/>
-#                 <input type='password' name='password' id='password' placeholder='password'/>
-#                 <input type='submit' name='submit'/>
-#                </form>
-#                '''
-#
-#     # email = flask.request.form['email']
-#     email = request.form.get('email', None)
-#     password = hashlib.md5(request.form.get('password', None)).hexdigest().upper()
-#
-#     print "EMAIL", email, "Pass", password, type(password)
-#     if email in users:
-#         if users[email]['password'] == password:
-#             user = User()
-#             user.id = email
-#             flask_login.login_user(user)
-#             return flask.redirect(flask.url_for('protected'))
-#         else:
-#             return "wrong password"
-#     else:
-#         return "user not found"
-#
-#     return 'Bad login'
-#
-#
+    def log(self):
+        lines = tailer.tail(open('logs/status.log'), 10)
 
-#
+        statement = ""
+
+        for line in lines:
+            statement += (line + "<br />")
+        return statement
+
+    def log_n(self, n):
+        lines = tailer.tail(open('logs/status.log'), n)
+
+        statement = ""
+
+        for line in lines:
+            statement += (line + "<br />")
+        return statement
+
+
+class User(flask_login.UserMixin):
+    # TODO: Implement this
+    pass
+
+
+if __name__ == '__main__':
+    pass
+
 # if __name__ == '__main__':
 #
 #
@@ -436,5 +401,3 @@ class flask_app(object):
 #     log = logging.getLogger('werkzeug')
 #     log.setLevel(logging.DEBUG)
 #     log.addHandler(handler)
-#
-#     app.run(host='0.0.0.0', debug=True)
--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-import numpy as np
-import cv2
-import json
-import sys
-import time
-import os
-from flask import Flask, render_template, Response, request, make_response
-
-import flask_login
-import flask
-
-from camera import VideoCamera
-from pose_detection import pose_detection
-from utils import utils
-
-import hashlib
-import logging
-from logging.handlers import RotatingFileHandler
-import tailer
-
 from app import flask_app
 
 
@@ -28,17 +8,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # rooms = parse_json("devices.json")
-    # users = parse_json("users.json")
-    # app.config["rooms"] = rooms
-    #
-    # handler = RotatingFileHandler('logs/status.log', maxBytes=10000, backupCount=99)
-    # formatter = logging.Formatter(
-    #     "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
-    # handler.setLevel(logging.DEBUG)
-    # handler.setFormatter(formatter)
-    # app.logger.addHandler(handler)
-    #
-    # log = logging.getLogger('werkzeug')
-    # log.setLevel(logging.DEBUG)
-    # log.addHandler(handler)
