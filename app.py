6be7cbdff5f55a49e211c5007b243dbd4c09dbb8
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2019-01-30 11:56:58 -0500

    add missing line

--- app.py
+++ app.py
@@ -270,6 +270,8 @@ class flask_app(object):
     @flask_login.login_required
     def compress_experiment(self, exp_id):
         exp_folder = self.um.experiment_path(str(exp_id))
+        exp_folder = os.path.join(os.path.dirname(
+            os.path.realpath(__file__)), exp_folder)
         archieve_name = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                      "backup", str(exp_id)+".zip")
         retval = self.um.compress_folder(exp_folder, archieve_name)
